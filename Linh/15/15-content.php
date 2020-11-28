b756d066cc5655d35d5650123a4bccc21371b7d9
PHAMCHELINH <phamchilinh15112016@gmail.com>
2020-11-28 16:46:05 +0700

    Cập nhật module lần 4

--- Linh/15/15-content.php
+++ Linh/15/15-content.php
@@ -12,29 +12,44 @@ $url_path = $url_host . $matches[1][0];
 $url_path = str_replace('\\', '/', $url_path);
 ?>
 <div class="type-15">
-    <div class="container sidebar">
+    <div class="container blog">
         <div class="row">
             <div class="col-md-9">
-                <div class="type-15-one">
+                <div class="one">
                     <a href="#">
-                        <img src="images/2.jpg" alt="blog2" class="type-15-img-responsive">
+                        <img src="images/1.jpg" alt="blog1" class="img-responsive">
                     </a>
-                    <div class="type-15-one_info clearfix">
-                        <div class="type-15-one_left">
-                        <ul>
-                                <li><i class="fa fa-calendar-o"></i></i>December 20, 2020
-                                    <em><a href="#" title="admin"rel="author">admin</a></em>
+                    <div class="one_info clearfix">
+                        <div class="one_left">
+                            <ul>
+                                <li>
+                                    <i class="fa fa-calendar-o"></i></i>December 20, 2020
+                                    <em><a href="#" title="admin" rel="author">admin</a></em>
                                 </li>
-                                <li></li>
-                                <li></li>
+                                <li>
+                                    <i class="fa fa-inbox"></i>
+                                    <a href="#">Category</a>
+                                </li>
+                                <li>
+                                    <i class="fa fa-tags"></i>
+                                    <a href="#">Dolor</a>,
+                                    <a href="#">Lorem ipsum</a>
+                                </li>
                             </ul>
                         </div>
-                        <div class="type-15-one_right">
-                            
+                        <div class="one_right">
+                            <i class="fa fa-comment-o"></i>
+                            <a href="#">No Commnets</a>
                         </div>
                     </div>
+                    <h2>Duis aute irure dolor in reprehenderit</h2>
+                    <p>Ludus albucius adversarium eam eu. Sit eu reque tation aliquip. Quo no dolorum albucius lucilius,
+                        hinc eligendi ut sed. Ex nam quot ferri suscipit, mea ne legere alterum repudiandae. Ei pri
+                        quaerendum intellegebat, ut vel consequuntur voluptatibus. Et volumus sententiae adversarium
+                        duo…… Ludus albucius adversarium eam eu. Sit eu...</p>
+                    <a href="#" class="button">Read more</a>
+                </div>
+                <!--/END RIGHT CONTENTS HOME-->
             </div>
-            <!--/END RIGHT CONTENTS HOME-->
         </div>
     </div>
-</div>
