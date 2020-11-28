f4a14dee72161b21fed15c7cb81834b2d8cb49f4
nguyentrihieu0660 <nguyentrihieu0660@gmail.com>
2020-11-28 16:08:55 +0700

    11
    111

---
+++ Hieu/18/18.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<?php
+
+$url_host = 'http://' . $_SERVER['HTTP_HOST'];
+$pattern_document_root = addcslashes(realpath($_SERVER['DOCUMENT_ROOT']), '\\');
+$pattern_uri = '/' . $pattern_document_root . '(.*)$/';
+preg_match_all($pattern_uri, __DIR__, $matches);
+$url_path = $url_host . $matches[1][0];
+$url_path = str_replace('\\', '/', $url_path);
+    if (!class_exists('lessc')) {
+        $dir_block = dirname($_SERVER['SCRIPT_FILENAME']);      
+        require_once($dir_block.'/libs/lessc.inc.php');
+    }
+    $less = new lessc;
+    $less->compileFile('less/18.less', 'css/18.css');
+    
+?>
+    
+
+<html lang="en-US">
+<head>
+    <meta charset="UTF-8">
+    <title>18</title>  
+    <link href="css/font-awesome.min.css" rel="stylesheet">       
+    <link href="css/18.css" rel="stylesheet" type="text/css" />
+    <script src="js/jquery-2.1.4.min.js"></script>
+    <script src="js/18.js"></script>  
+      <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"> 
+    
+
+</head>
+
+<body>
+    <?php include $dir_block.'/18-content.php'; ?>
+    
+
+</body>
