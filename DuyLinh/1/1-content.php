6d8caa84f940feb382e1b21e99b60b0b107e495d
huynhvuonghihi <huynhdovuongb11@gmail.com>
2021-01-09 13:18:27 +0700

    duy linh may hu
    duy linh may hu

--- DuyLinh/1/1-content.php
+++ DuyLinh/1/1-content.php
@@ -10,7 +10,7 @@ $url_path = str_replace('\\', '/', $url_path);
 
 <div class="type-1">
 	<div class="header">
-		<nav class="navbar navbar-expand-sm bg-dark navbar-dark">
+		<nav class="navbar navbar-expand-sm navbar-dark">
 			<!-- Brand -->
 			<a class="navbar-brand" href="#">
 				<img src="https://shtheme.com/demosd/travelguidewp/wp-content/themes/travelguide/img/logo.png" width="170" height="30" alt="" data-retina="true">
@@ -78,5 +78,40 @@ $url_path = str_replace('\\', '/', $url_path);
 			
 		</nav>
 	</div>
-	
+	<section class="header-video" style="width: 1263px; height: 623px;">
+
+		<div id="hero_video">
+
+			<div id="sub_content_in">
+
+				<h1>The most important info<br>
+				on your Mobile during a visit</h1>
+
+				<p>
+
+					Museum Guides / Directions / Tips&amp;info
+				</p>
+
+
+
+			</div>
+
+			<!-- End sub_content -->
+
+		</div>
+
+		<img src="https://shtheme.com/demosd/travelguidewp/wp-content/uploads/2017/02/video_fix.png" alt="" class="header-video--media" data-video-src="https://shtheme.com/demosd/travelguidewp/wp-content/themes/travelguide/video/intro" data-teaser-source="https://shtheme.com/demosd/travelguidewp/wp-content/themes/travelguide/video/intro" data-provider="" data-video-width="1920" data-video-height="960" style="display: none;">
+
+		<video autoplay="true" loop="loop" muted="" id="teaser-video" class="teaser-video"><source src="https://shtheme.com/demosd/travelguidewp/wp-content/themes/travelguide/video/intro.mp4" type="video/mp4"><source src="https://shtheme.com/demosd/travelguidewp/wp-content/themes/travelguide/video/intro.ogv" type="video/ogg"></video></section>
 </div>
+<script>
+    $(document).ready(function(){
+      $(window).scroll(function() { // check if scroll event happened
+        if ($(document).scrollTop() > 50) { // check if user scrolled more than 50 from top of the browser window
+          $(".navbar").css("background-color", "#222222"); // if yes, then change the color of class "navbar-fixed-top" to white (#f8f8f8)
+        } else {
+          $(".navbar").css("background-color", "transparent"); // if not, change it back to transparent
+        }
+      });
+    });
+</script>
